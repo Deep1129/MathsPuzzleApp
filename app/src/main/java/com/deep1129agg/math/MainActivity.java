a67f480825f7896b3b856e8aa37e5eb15326f240
Deepanshu <deep1129agg@gmail.com>
2019-02-15 03:24:14 +0530

    You Won! message after successful achivement of target. Invalid move Toast for invalid moves.

--- app/src/main/java/com/deep1129agg/math/MainActivity.java
+++ app/src/main/java/com/deep1129agg/math/MainActivity.java
@@ -15,28 +15,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     TextView textView1, textView2,textView3,textView4,textView5,textView6,textView7,textView8,
             textView9,textView10,textView11,textView12,textView13,textView14, textView15, textView16;
-    TextView resultTextView;
+    TextView resultTextView, gameResultTextView;
     TextView[] textViewArray;
-
+    public static boolean gameOn;
     boolean textView1Boolean, textView2Boolean,textView3Boolean,textView4Boolean,textView5Boolean,textView6Boolean
             ,textView7Boolean,textView8Boolean,textView9Boolean,textView10Boolean,textView11Boolean,textView12Boolean,
             textView13Boolean,textView14Boolean, textView15Boolean, textView16Boolean;
     String[] entry = {"1","2","3","4","5","6","7","8","9","2","-","*","/","%","+"};
     int noOfClicks = 0, prevTag = 0;
-
     String[] textViewforResult = new String[3];
     int[] resultTextViewTag = new int[3];
     int[] resultTextViewIndex = new int[3];
-    int operatorCount=0;
-
+    int operatorCount=0,r;
     boolean[] textViewBooleanArray = new boolean[16];
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
 
         textView1 = findViewById(R.id.textView1);
         textView2 = findViewById(R.id.textView2);
@@ -55,6 +51,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         textView15 = findViewById(R.id.textView15);
         textView16 = findViewById(R.id.textView16);
         resultTextView = findViewById(R.id.resultTextView);
+        gameResultTextView = findViewById(R.id.gameResultTextView);
         textViewArray = new TextView[16];
         textViewArray[0] = textView1;
         textViewArray[1] = textView2;
@@ -90,22 +87,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         textViewBooleanArray[14] = textView15Boolean;
         textViewBooleanArray[15] = textView16Boolean;
 
+        gameOn = true;
 
-
-        for(int i = 0 ; i<16; i++) {
-            textViewArray[i].setOnClickListener(this);
-
-        }
-
-
+//        for(int i = 0 ; i<16; i++) {
+//            textViewArray[i].setOnClickListener(this);
+//
+//        }
 
         for (int i = 0; i<16; i++) {
+            textViewArray[i].setOnClickListener(this);
             fillEntry(i);
             textViewBooleanArray[i]=false;
         }
-
         setResultValue();
-
     }
 
     public void fillEntry(int i){
@@ -116,60 +110,60 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     public void setResultValue(){
         Random rand = new Random();
-        int r = rand.nextInt(25)+25;
+        r = rand.nextInt(25)+25;
         resultTextView.setText(String.valueOf(r));
 
     }
 
     @Override
     public void onClick(View v) {
+        if(gameOn == true) {
+            String viewTag = v.getTag().toString();
+            int intTag = Integer.valueOf(viewTag);
+            for (int i = 0; i < 16; i++) {
+                if (i == intTag) {
+                    if (prevTag == 0 || intTag == prevTag + 1 || intTag == prevTag - 1 || intTag == prevTag + 4 || intTag == prevTag - 4
+                            || prevTag == intTag) {
+                        if (textViewBooleanArray[i] == false) {
+                            Log.i("CLicked", viewTag + " ");
+                            textViewforResult[noOfClicks] = textViewArray[intTag].getText().toString();
+                            resultTextViewTag[noOfClicks] = intTag;
+                            if (textViewforResult[noOfClicks].equals("-")) {
+                                resultTextViewIndex[noOfClicks] = 10;
+                            } else if (textViewforResult[noOfClicks].equals("*")) {
+                                resultTextViewIndex[noOfClicks] = 11;
+                            } else if (textViewforResult[noOfClicks].equals("/")) {
+                                resultTextViewIndex[noOfClicks] = 12;
+                            } else if (textViewforResult[noOfClicks].equals("%")) {
+                                resultTextViewIndex[noOfClicks] = 13;
+                            } else if (textViewforResult[noOfClicks].equals("+")) {
+                                resultTextViewIndex[noOfClicks] = 14;
+                            } else {
+                                resultTextViewIndex[noOfClicks] = Integer.valueOf(textViewArray[intTag].getText().toString());
+                            }
 
-        Log.i("CLicked", "successful");
+                            noOfClicks++;
+                            textViewArray[i].setBackgroundColor(Color.rgb(165, 143, 250));
+                            textViewBooleanArray[i] = true;
+                            if (noOfClicks == 3) {
+                                calculateResult(resultTextViewTag, textViewforResult);
+                            } else {
+                                prevTag = intTag;
+                                //                    will fix this later.
 
-        String viewTag = v.getTag().toString();
-        int intTag = Integer.valueOf(viewTag);
-        for(int i = 0;i<16; i++){
-            if(i == intTag ){
-                if(prevTag==0 || intTag == prevTag+1 || intTag == prevTag-1 || intTag == prevTag+4 || intTag == prevTag-4
-                || prevTag==intTag) {
-                    if (textViewBooleanArray[i] == false) {
-                        Log.i("CLicked", viewTag + " ");
-                        textViewforResult[noOfClicks] = textViewArray[intTag].getText().toString();
-                        resultTextViewTag[noOfClicks]= intTag;
-                        if(textViewforResult[noOfClicks].equals("-")){
-                            resultTextViewIndex[noOfClicks] = 10;
-                        }else if(textViewforResult[noOfClicks].equals("*")){
-                            resultTextViewIndex[noOfClicks] = 11;
-                        }else if(textViewforResult[noOfClicks].equals("/")){
-                            resultTextViewIndex[noOfClicks] = 12;
-                        }else if(textViewforResult[noOfClicks].equals("%")){
-                            resultTextViewIndex[noOfClicks] = 13;
-                        }else if(textViewforResult[noOfClicks].equals("+")){
-                            resultTextViewIndex[noOfClicks] = 14;
-                        }else{
-                            resultTextViewIndex[noOfClicks] = Integer.valueOf(textViewArray[intTag].getText().toString());
+                            }
+
+                        } else {
+                            textViewArray[i].setBackgroundColor(Color.rgb(255, 237, 217));
+                            noOfClicks--;
+                            textViewBooleanArray[i] = false;
                         }
 
-                        noOfClicks++;
-                        textViewArray[i].setBackgroundColor(Color.rgb(165, 143, 250));
-                        textViewBooleanArray[i] = true;
-                        if(noOfClicks == 3){
-                            calculateResult(resultTextViewTag,textViewforResult);
-                        }else {
-                            prevTag= intTag;
-                            //                    will fix this later.
-
-                        }
-
-                    } else {
-                        textViewArray[i].setBackgroundColor(Color.rgb(255, 237, 217));
-                        noOfClicks--;
-                        textViewBooleanArray[i] = false;
                     }
 
                 }
-
             }
+
         }
 
     }
@@ -184,22 +178,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             textViewArray[i].setBackgroundColor(Color.rgb(255, 237, 217));
             textViewBooleanArray[i]=false;
         }
-        for(int i =0; i<3;i++){
-            if(textViewforResult[i].equals("-")){
-                operator = 10;
-            }else if(textViewforResult[i].equals("*")){
-                operator = 11;
-            }else if(textViewforResult[i].equals("/")){
-                operator = 12;
-            }else if(textViewforResult[i].equals("%")){
-                operator = 13;
-            }else if(textViewforResult[i].equals("+")){
-                operator = 14;
+        try {
+            for (int i = 0; i < 3; i++) {
+                if (textViewforResult[i].equals("-")) {
+                    operator = 10;
+                } else if (textViewforResult[i].equals("*")) {
+                    operator = 11;
+                } else if (textViewforResult[i].equals("/")) {
+                    operator = 12;
+                } else if (textViewforResult[i].equals("%")) {
+                    operator = 13;
+                } else if (textViewforResult[i].equals("+")) {
+                    operator = 14;
+                } else {
+                    operand[k++] = Integer.valueOf(textViewforResult[i]);
+                }
             }
-            else{
-                operand[k++] = Integer.valueOf(textViewforResult[i]);
+            if(k!=2){
+                throw new Exception();
             }
-        }
 
         if(operator == 10){
             result = Math.abs(operand[0]-operand[1]);
@@ -225,7 +222,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         fillEntry(resultTextViewTag[1]);
         textViewArray[resultTextViewTag[2]].setText(String.valueOf(result));
 
-
         if(noOfOperators() == 0){
             Toast.makeText(this, "No more moves available", Toast.LENGTH_SHORT).show();
 
@@ -233,30 +229,33 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 fillEntry(i);
                 textViewBooleanArray[i]=false;
             }
-
         }
-
-
         Log.i("result", result+" ");
+        }catch (Exception e){
+            Toast.makeText(this, "Invalid move", Toast.LENGTH_SHORT).show();
+        }
     }
 
     public void playAgainButton(View view){
         for (int i = 0; i<16; i++) {
             fillEntry(i);
             textViewBooleanArray[i]=false;
+            gameOn=true;
+            gameResultTextView.setText(" ");
         }
-
         setResultValue();
-
     }
 
     public int noOfOperators(){
         operatorCount=0;
         for(int i = 0; i<16; i++ ){
             String value = textViewArray[i].getText().toString();
-            if(value.equals("+") || value.equals("-") || value.equals("*") || value.equals("/") || value.equals("%") ){
+           if(value.equals("+") || value.equals("-") || value.equals("*") || value.equals("/") || value.equals("%") ){
                 operatorCount++;
-            }
+            } else if(r == Integer.valueOf(value)){
+               gameOn=false;
+               gameResultTextView.setText("You Won!");
+           }
         }
         return operatorCount;
     }
